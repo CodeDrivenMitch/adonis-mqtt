c33af9e54f664cb6d21a627bdaafcb25b0dcd9ec
Will Vincent <will@willvincent.com>
2018-07-26 17:38:59 -0500

    Fixing instructions.js
    Ensure copy of config file & creation of mqttlisteners directory as needed.

--- instructions.js
+++ instructions.js
@@ -1,14 +1,21 @@
 'use strict'
 
 const path = require('path')
+const fs = require('fs')
 
 module.exports = async function (cli) {
   try {
     await cli.makeConfig('mqtt.js', path.join(__dirname, './templates/config.js'))
     cli.command.completed('create', 'config/mqtt.js')
 
-    await cli.ensureDir(cli.helpers.appRoot('MqttListeners'));
+
+    let dir = path.join(cli.appDir, './MqttListeners');
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+      cli.command.completed('created directory', 'app/MqttListeners')
+    }
   } catch (error) {
+    console.log(error)
     // ignore errors
   }
 }
